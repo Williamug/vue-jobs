2ea245da696d1b59e9c47287650331dfc8d2d1e3
Asaba William <asabawilliamdk@yahoo.com>
2024-07-06 23:55:59 +0300

    wip

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+</script>
+<template>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+</template>
