0bf8b486c6bc377fde063fbb7dfe3435bb350358
Asaba William <asabawilliamdk@yahoo.com>
2024-07-10 12:45:30 +0300

    Add job listing

---
+++ src/components/JobListing.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+    <div class="relative bg-white shadow-md rounded-xl">
+        <div class="p-4">
+            <div class="mb-6">
+                <div class="my-2 text-gray-600">Full-Time</div>
+                <h3 class="text-xl font-bold">Senior Vue Developer</h3>
+            </div>
+
+            <div class="mb-5">
+                We are seeking a talented Front-End Developer to join our team
+                in Boston, MA. The ideal candidate will have strong skills in
+                HTML, CSS, and JavaScript...
+            </div>
+
+            <h3 class="mb-2 text-green-500">$70 - $80K / Year</h3>
+
+            <div class="mb-5 border border-gray-100"></div>
+
+            <div class="flex flex-col justify-between mb-4 lg:flex-row">
+                <div class="mb-3 text-orange-700">
+                    <i class="text-lg fa-solid fa-location-dot"></i>
+                    Boston, MA
+                </div>
+                <a href="job.html"
+                    class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
+                    Read More
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
