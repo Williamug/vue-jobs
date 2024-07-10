05eb13500dd511bc7f9a229d8896b7301eb6a379
Asaba William <asabawilliamdk@yahoo.com>
2024-07-10 12:45:19 +0300

    Import joblistings

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
 import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero />
   <HomeCards />
+  <JobListings />
 </template>
